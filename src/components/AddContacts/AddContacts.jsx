cea877128926bda3d5c97b67fe35e14beb84e5d9
VitaliyYakovenko <yakovenkovitaliy1@gmail.com>
2025-03-24 21:03:20 +0200

    add clear redux

--- src/components/AddContacts/AddContacts.jsx
+++ src/components/AddContacts/AddContacts.jsx
@@ -70,7 +70,7 @@ export default function AddContacts({onGetData}) {
             <br/>
             <BtnAddContact type="submit">Add Contact</BtnAddContact> 
             </form>)
-    };
+};
 
 
 
@@ -78,70 +78,3 @@ export default function AddContacts({onGetData}) {
 
 
 
-// import React, { Component } from "react";
-// import { nanoid } from "nanoid";
-// import { Label,Input, BtnAddContact} from "./AddContacts.styled";
-
-
-
-// export default class AddContacts extends Component {
-//     nameId = nanoid();
-//     telId = nanoid();
-
-
-//     state = {
-//         name: "",
-//         number: "",
-//     }
-
-//     onInputName = (e) => {
-        
-//         const name = e.target.name;
-//         const value = e.target.value;
-    
-//         this.setState(prevState => ({
-//             [name]: value,
-//         }));
-//     }
-   
-//     onAddContact = (e) => {
-//         e.preventDefault();
-    
-//         this.props.onGetData(this.state);
-//         this.reset();
-        
-//     }
-
-//     reset = () => {
-//         this.setState({ name: "" , number: ""});
-//     }
-
-//     render() {
-    
-//         return (<form onSubmit={this.onAddContact}>
-//             <Label htmlFor={this.nameId}>Name</Label>
-//             <br/>
-//             <Input
-//             onChange={this.onInputName}    
-//             id={this.nameId} 
-//             value={this.state.name}    
-//             type="text"
-//             name="name"
-//             required
-//             />
-//             <br/>
-//             <Label htmlFor={this.telId}>Number</Label>
-//             <br/>
-//             <Input
-//             onChange={this.onInputName}    
-//             id={this.telId}
-//             value={this.state.number}    
-//             type="tel"
-//             name="number"
-//             required
-//             />
-//             <br/>
-//             <BtnAddContact type="submit">Add Contact</BtnAddContact> 
-//             </form>)
-//     };
-// };
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,22 @@
-import  { useState, useEffect } from "react";
-import { Container,Title ,TitleContacts} from "./App.styled";
+import { Container, Title, TitleContacts } from "./App.styled";
+import { useSelector, useDispatch } from "react-redux";
+import { nanoid } from "nanoid";
+import { addContact, deleteContact } from "../../redux/contactsSlice";
+import { filteredContacts } from "../../redux/filterSlice";
 import AddContacts from "../AddContacts/AddContacts";
 import FilteredContacts from "../FilteredContacts/FilteredContacts";
 import ContactsList from "../ContactsList/ContactsList";
-import { nanoid } from "nanoid";
-const LS_KEY = "contacts";
+
 
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const value = localStorage.getItem(LS_KEY);
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+ 
 
-    return value ? JSON.parse(value) : [];
-  });
-  const [filter, setFilter] = useState("");
-
-
-  useEffect(() => {
-    const data = localStorage.getItem(LS_KEY);
-    
-    if (data) {
-      setContacts(JSON.parse(data));
-    };
-   }, []);
-
-
-  useEffect(() => {
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-   }, [contacts]);
   
-  
-  
-   const  addNewContact = ({ name, number }) => {
+  const  addNewContact = ({ name, number }) => {
     
     const id = nanoid();
     const newContact = {
@@ -47,25 +32,27 @@ export default function App() {
       return;
      };
  
-     setContacts((prev) => [newContact, ...prev]);
+     dispatch(addContact(newContact));
+  };
+ 
+  const onDeleteContact = (id) => {
+    dispatch(deleteContact(id));
   };
 
 
   const onChangeFilter = (e) => {
-    setFilter(e.currentTarget.value);
+    const string = e.currentTarget.value;
+    dispatch(filteredContacts(string));
   };
 
 
-  const onDeleteContact = (id) => {
-    setContacts(prev => prev.filter(contact => contact.id !== id));
-  };
+  const normalaizeFilter = filter?.toLocaleLowerCase();
 
-
-  const normalaizeFilter = filter.toLocaleLowerCase();
 
   const filteredArr = contacts.filter(contact => {
-    return contact.name.toLowerCase().includes(normalaizeFilter);
+    return contact.name?.toLowerCase().includes(normalaizeFilter);
   });
+
 
    
     return (<Container>
@@ -80,111 +67,6 @@ export default function App() {
         data={filteredArr} />
     </Container>)
   };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from "react";
-// import { Container,Title ,TitleContacts} from "./App.styled";
-// import AddContacts from "../AddContacts/AddContacts";
-// import FilteredContacts from "../FilteredContacts/FilteredContacts";
-// import ContactsList from "../ContactsList/ContactsList";
-// import { nanoid } from "nanoid";
-// const LS_KEY = "contacts";
-
-
-// export default class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: "",
-//   };
- 
-//   componentDidMount() {
-//     const data = localStorage.getItem(LS_KEY);
-//     if (data) {
-//       this.setState(({contacts: JSON.parse(data)}));
-//     }
-//   };
-
-//   componentDidUpdate(_, prevState) {
-//     if (prevState.contacts !== this.state.contacts) {
-//       localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
-//     }
-//   };
-
-
-//   addNewContact = ({ name, number }) => {
-//     const { contacts } = this.state;
-//     const id = nanoid();
-//     const newContact = {
-//       id,
-//       name,
-//       number
-//     };
-
-//     const isFind = contacts.find((el) => el.name === name);
-   
-//     if (isFind) {
-//       alert("go home mazafacka!!!");
-//       return;
-//     }
-   
-
-//     this.setState(({ contacts }) => ({
-//       contacts: [newContact, ...contacts]
-//     }));
-//   };
-
-
-//   onChangeFilter = (e) => {
-//     this.setState({ filter: e.currentTarget.value });
-//   };
-
-
-//   onDeleteContact = (id) => {
-
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(contact => contact.id !== id)   
-//     }));
-//   };
-
-
-//   render() {
-//     const { contacts, filter } = this.state;
-//     const normalaizeFilter = filter.toLocaleLowerCase();
-
-//     const filteredArr = contacts.filter(contact => {
-//       return contact.name.toLowerCase().includes(normalaizeFilter);
-//     });
-   
-//     return (<Container>
-//       <Title>Phonebook</Title>
-//       <AddContacts onGetData={this.addNewContact}/>    
-//       <TitleContacts>Contacts</TitleContacts>
-//       <FilteredContacts
-//         onFilter={this.onChangeFilter}
-//         sort={filter} /> 
-//       <ContactsList
-//         onDeleteById={this.onDeleteContact}
-//         data={filteredArr} />
-//     </Container>)
-//   }
-// };
-
-
-
-
 
 
 
